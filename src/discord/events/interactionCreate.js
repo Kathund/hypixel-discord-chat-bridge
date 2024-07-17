e1def2bcc5770ff5e7f3bf7d7073f1390acff994
Kath <admin@kath.lol>
2024-07-17 12:07:31 +0800

    fix quicksetup merge

--- src/discord/events/interactionCreate.js
+++ src/discord/events/interactionCreate.js
@@ -21,8 +21,8 @@ module.exports = {
         }
 
         const memberRoles = interaction.member.roles.cache.map((role) => role.id);
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
-        if (memberRoles.some((role) => config.discord.commands.blacklistRoles.includes(role))) {
+        await interaction.deferReply({ ephemeral: command.ephemeral || false }).catch(() => {});
+        if (memberRoles.includes(config.discord.commands.blacklistRole)) {
           throw new HypixelDiscordChatBridgeError("You are blacklisted from the bot.");
         }
 
