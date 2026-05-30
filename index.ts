4f623cfcce03b25e2399049c619f012f3863c0af
Jacob <kathund@proton.me>
2026-05-30 17:54:19 +0800

    fix: running with old config errored
    ConfigManager now gets ran first before the application gets loaded
    meaning the config gets moved to the new version with the correct values
    first
    
    Co-authored-by: Pixelic <69585766+Pixelicc@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import Application from "./src/Application.js";
 import ConfigManager from "./src/ConfigManager.js";
 import "./src/private/logger.js";
 
 new ConfigManager();
+
+const { default: Application } = await import("./src/Application.js");
 new Application().connect();
